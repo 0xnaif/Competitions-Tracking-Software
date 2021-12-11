3650a863808fe14acb25384351dbde2a46165378
نواف ال فواز <63208501+NawaF911n@users.noreply.github.com>
2021-12-11 03:42:50 +0300

    Update CompetitionTB.java

--- CompetitionTB.java
+++ CompetitionTB.java
@@ -1,54 +1,20 @@
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.Date;
 
-public class Competition {
-	private LocalDate date;
-	private String link;
-	private String name;
-	private String type;
-	private String status;
+public class CompetitionTB extends Competition {
+	private ArrayList<Team> teams;
 
-	public Competition(String link, String name,String type) {
-		this.type = type;
-		this.link = link;
-		this.name = name;
+	public CompetitionTB(String link, String name) {
+		super(link, name,"Team Basd");
+		teams = new ArrayList<Team>();
 	}
-	
-	public String getName() {
-
-		return name;
-	}
-	public String getType() {
-
-		return type;
-	}
-	public String getLink() {
-		return link;
-	}
-	public String getStatus() {
-		return status;
-	}
-	public LocalDate getDate() {
-		return date;}
 	public void setDate(int year, int month,int day) {
-		this.date = LocalDate.of(year, month, day);
-		try {updateStatus();}
-		catch (ParseException e) {}}
-	public void updateStatus() throws ParseException {
-		String year = this.date.getYear()+"-";
-		String month = this.date.getMonthValue()+"-";
-		String day = this.date.getDayOfMonth()+"";
-		
-		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd"); 
-    	Date date = sdf.parse(year+month+day);
-    	Date dateToday = new Date();
-
-		if(date.compareTo(dateToday) > 0)   
-			this.status = "On";   
-		else 
-			this.status = "Off";
-			
+		super.setDate(year, month, day);
+	}
+	public void addTeam(Team team) {
+		teams.add(team);
+	}
+	public ArrayList<Team> getTeams() {
+		return teams;
 	}
 }
