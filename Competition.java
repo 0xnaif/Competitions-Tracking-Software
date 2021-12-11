a868d27cfe375039ad02e872fd26c948a1e741c9
نواف ال فواز <63208501+NawaF911n@users.noreply.github.com>
2021-12-11 03:44:18 +0300

    Update Competition.java

--- Competition.java
+++ Competition.java
@@ -1,10 +1,14 @@
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.time.LocalDate;
+import java.util.Date;
 
 public class Competition {
 	private LocalDate date;
 	private String link;
 	private String name;
 	private String type;
+	private String status;
 
 	public Competition(String link, String name,String type) {
 		this.type = type;
@@ -21,14 +25,29 @@ public class Competition {
 		return type;
 	}
 	public String getLink() {
-
 		return link;
 	}
-	public void setDate(int year, int month,int day) {
-		this.date = LocalDate.of(year, month, day);
+	public String getStatus() {
+		return status;
 	}
 	public LocalDate getDate() {
+		return date;}
+	public void setDate(int year, int month,int day) {
+		this.date = LocalDate.of(year, month, day);
+		try {updateStatus();}
+		catch (ParseException e) {}}
+	public void updateStatus() throws ParseException {
+		String year = this.date.getYear()+"-";
+		String month = this.date.getMonthValue()+"-";
+		String day = this.date.getDayOfMonth()+"";
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd"); 
+    	Date date = sdf.parse(year+month+day);
+    	Date dateToday = new Date();
 
-		return date;
+		if(date.compareTo(dateToday) > 0)   
+			this.status = "On";   
+		else 
+			this.status = "Off";
 	}
 }
