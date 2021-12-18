ad8cca8c66803e8cb94fba60131f5916bb58281b
Naif <48796390+r0o0tx@users.noreply.github.com>
2021-12-18 16:30:22 +0300

    Update CompetitionTB.java

--- CompetitionTB.java
+++ CompetitionTB.java
@@ -1,19 +1,22 @@
+
 import java.util.ArrayList;
-import java.util.Date;
 
 public class CompetitionTB extends Competition {
 	private ArrayList<Team> teams;
 
 	public CompetitionTB(String link, String name) {
-		super(link, name,"Team Basd");
+		super(link, name, "Team Based");
 		teams = new ArrayList<Team>();
 	}
-	public void setDate(int year, int month,int day) {
+
+	public void setDate(int year, int month, int day) {
 		super.setDate(year, month, day);
 	}
+
 	public void addTeam(Team team) {
 		teams.add(team);
 	}
+
 	public ArrayList<Team> getTeams() {
 		return teams;
 	}
