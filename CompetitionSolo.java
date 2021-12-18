56a2f6480a0872a30991f983094bd904bc099e15
Naif <48796390+r0o0tx@users.noreply.github.com>
2021-12-18 16:30:04 +0300

    Update CompetitionSolo.java

--- CompetitionSolo.java
+++ CompetitionSolo.java
@@ -1,17 +1,18 @@
-import java.util.ArrayList;
 
+import java.util.ArrayList;
 
 public class CompetitionSolo extends Competition {
 	private ArrayList<Student> individuals;
 
 	public CompetitionSolo(String link, String name) {
-		super(link, name,"Solo");
+		super(link, name, "Solo");
 		individuals = new ArrayList<Student>();
 	}
 
-	public void setDate(int year, int month,int day) {
+	public void setDate(int year, int month, int day) {
 		super.setDate(year, month, day);
 	}
+
 	public void addStudent(Student student) {
 		individuals.add(student);
 	}
